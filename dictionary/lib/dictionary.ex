ce8a602941617c26a28b44e7257713374e4091b2
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-02-17 12:49:53 -0300

    moved challenges to proper repository

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -9,20 +9,4 @@ defmodule Dictionary do
     @word_list
     |> Enum.random()
   end
-
-  # challenges from the course Pattern Matching section
-
-  def func(_t = { a, b }) do
-    IO.puts("a = #{a}, b = #{b}, is_tuple{t}")
-  end
-
-  def swap_values({a, b}), do: {b, a}
-
-  def same(a, a), do: true
-  def same(_, _), do: false
-
-  def len([]), do: 0
-  def len([_head|tail]), do: 1 + len(tail)
-
-  # end of the Pattern Matching challenges
 end
